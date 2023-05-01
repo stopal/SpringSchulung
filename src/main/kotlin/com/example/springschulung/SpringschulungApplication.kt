89d15bc595187e85b01df1c49908651703941971
Serkan Topaloglu <s.topaloglu@tarent.de>
2023-05-01 17:57:24 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/springschulung/SpringschulungApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springschulung
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringschulungApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringschulungApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springschulung/SpringschulungApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springschulung
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringschulungApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
